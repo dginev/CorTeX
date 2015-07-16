c4b0a4035a71b4d725cce818ac3667db5cfe7371
Deyan Ginev <d.ginev@jacobs-university.de>
2015-07-15 20:16:46 -0400

    properly testing unpack of arxiv

--- src/importer.rs
+++ src/importer.rs
@@ -6,6 +6,7 @@ use regex::Regex;
 use Archive::*;
 use std::path::Path;
 use std::fs;
+use std::fs::File;
 
 // Only initialize auxiliary resources once and keep them in a Importer struct
 pub struct Importer <'a> {
@@ -78,25 +79,49 @@ impl <'a> Importer <'a> {
       match entry {
         Ok(path) => {
           let entry_path = path.to_str().unwrap();
+          let entry_dir = path.parent().unwrap().to_str().unwrap();
+          let base_name = path.file_stem().unwrap().to_str().unwrap();
+          let entry_cp_dir = entry_dir.to_string() + "/" + base_name;
+          fs::create_dir_all(entry_cp_dir.clone()).unwrap_or_else( |reason| {
+            println!("Failed to mkdir -p {:?} because: {:?}", entry_cp_dir.clone(), reason.kind());
+          });
           // Careful here, some of arXiv's .gz files are really plain-text TeX files (surprise!!!)
           let archive_reader_new = Reader::new().unwrap()
             .support_filter_all()
             .support_format_all()
             .open_filename(entry_path, 10240);
+          // We'll write out a ZIP file for each entry
+          let archive_writer_new = Writer::new().unwrap();
           match archive_reader_new {
-            Err(_) => {
-              println!("Simple TeX file: {:?}", entry_path);
+            Err(e) => {
+              let raw_reader_new = Reader::new().unwrap()
+                .support_filter_all()
+                .support_format_raw()
+                .open_filename(entry_path, 10240);
+              match raw_reader_new {
+                Ok(raw_reader) => {
+                  println!("Simple TeX file: {:?}", entry_path);
+                  match raw_reader.next_header() {
+                    Ok(e) => {
+                      let full_extract_path = entry_cp_dir.to_string() + "/" + base_name + ".tex";
+                      e.extract_to(&full_extract_path);
+                    },
+                    Err(_) => println!("No content in archive: {:?}", entry_path)
+                  }
+                },
+                Err(_) => println!("Unrecognizeable archive: {:?}", entry_path)
+              }
             },
             Ok(archive_reader) => {
               loop {
                 match archive_reader.next_header() {
                   Ok(e) => {
-                    let full_extract_path = path_str.to_string() + &e.pathname();
+                    let full_extract_path = entry_cp_dir.to_string() + "/" + &e.pathname();
                     match fs::metadata(full_extract_path.clone()) {
                       Ok(m) => println!("File {:?} exists, won't unpack.", e.pathname()),
                       Err(_) => {
                         println!("To unpack: {:?}", full_extract_path); 
-                        // let res_code = e.extract_to(&full_extract_path);
+                        let res_code = e.extract_to(&full_extract_path);
                       }
                     }
                   },
@@ -105,6 +130,8 @@ impl <'a> Importer <'a> {
               }
             }
           }
+          // Done with this .gz , remove it:
+          fs::remove_file(path.clone());
         },
         Err(e) => println!("Failed gz glob: {:?}", e)
       }
--- tests/importer_test.rs
+++ tests/importer_test.rs
@@ -1,5 +1,28 @@
 extern crate cortex;
 use cortex::importer::*;
+use std::vec::Vec;
+use std::fs;
+use std::io::{Error};
+
+fn assert_files(files:Vec<&str>) -> Result<(),std::io::Error> {
+  for file in files.iter() {
+    let meta = fs::metadata(file.clone());
+    assert!(meta.is_ok());
+    assert!(meta.unwrap().is_file());
+    // They're also temporary, so delete them
+    try!(fs::remove_file(file.clone())); 
+  }
+  Ok(()) }
+
+fn assert_dirs(dirs : Vec<&str>) -> Result<(),std::io::Error> {
+  for dir in dirs.iter() {
+    let meta = fs::metadata(dir.clone());
+    assert!(meta.is_ok());
+    assert!(meta.unwrap().is_dir());
+    // They're also temporary, so delete them
+    try!(fs::remove_dir(dir.clone())); 
+  }
+  Ok(()) }
 
 #[test]
 fn can_import_simple() {
@@ -19,4 +42,17 @@ fn can_import_complex() {
   
   println!("-- Testing complex import");
   assert_eq!( importer.process(), Ok(()) );
+  let files_removed_ok = assert_files(vec![
+    "tests/data/9107/hep-lat9107001/9107001.tex",
+    "tests/data/9107/hep-lat9107001/fig1.eps",
+    "tests/data/9107/hep-lat9107002/hep-lat9107002.tex"
+    ]);
+  assert!(files_removed_ok.is_ok());
+  let dirs_removed_ok = assert_dirs(vec![
+    "tests/data/9107/hep-lat9107001",
+    "tests/data/9107/hep-lat9107002",
+    "tests/data/9107", 
+  ]);
+  assert!(dirs_removed_ok.is_ok());
+  
 }
