97f011dd90d913632d02eb4f9c7d24a06be32532
Deyan Ginev <d.ginev@jacobs-university.de>
2015-09-28 20:08:10 -0400

    why was the sleep placed in such a wrong place? fix.

--- tests/tex_to_html_test.rs
+++ tests/tex_to_html_test.rs
@@ -81,11 +81,11 @@ fn mock_tex_to_html() {
   let worker = TexToHtmlWorker::default();
   // Perform a single echo task 
   assert!(worker.start(Some(1)).is_ok());
+  // Wait for the finisher to persist to DB 
+  thread::sleep_ms(2001); // TODO: Can this be deterministic? Join?
   // Check round-trip success
   let finished_task = test_backend.sync(&conversion_task).unwrap();
   println!("Finished: {:?}", finished_task);
-  // Wait for the finisher to persist to DB 
-  thread::sleep_ms(3001); // TODO: Can this be deterministic? Join?
   // This particular test finishes with an Error with the current LaTeXML (needs cmp.sty).
   assert!(finished_task.status == TaskStatus::Error.raw())
 }
