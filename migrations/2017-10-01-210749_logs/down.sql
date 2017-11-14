6c42dcbd3bf853150cce090f136504af26df8f18
Deyan Ginev <deyan.ginev@gmail.com>
2017-11-13 22:17:50 -0500

    add log_infos table for completeness of message provenance

--- migrations/2017-10-01-210749_logs/down.sql
+++ migrations/2017-10-01-210749_logs/down.sql
@@ -1,4 +1,5 @@
-DROP TABLE log_fatals;
-DROP TABLE log_errors;
+DROP TABLE log_infos;
 DROP TABLE log_warnings;
+DROP TABLE log_errors;
+DROP TABLE log_fatals;
 DROP TABLE log_invalids;
--- migrations/2017-10-01-210749_logs/up.sql
+++ migrations/2017-10-01-210749_logs/up.sql
@@ -1,4 +1,11 @@
-CREATE TABLE log_fatals (
+CREATE TABLE log_infos (
+  messageid BIGSERIAL PRIMARY KEY,
+  taskid BIGINT NOT NULL,
+  category varchar(50),
+  what varchar(50),
+  details varchar(2000)
+);
+CREATE TABLE log_warnings (
   messageid BIGSERIAL PRIMARY KEY,
   taskid BIGINT NOT NULL,
   category varchar(50),
@@ -12,7 +19,7 @@ CREATE TABLE log_errors (
   what varchar(50),
   details varchar(2000)
 );
-CREATE TABLE log_warnings (
+CREATE TABLE log_fatals (
   messageid BIGSERIAL PRIMARY KEY,
   taskid BIGINT NOT NULL,
   category varchar(50),
@@ -27,13 +34,15 @@ CREATE TABLE log_invalids (
   details varchar(2000)
 );
 
-create index log_fatals_taskid on log_fatals(taskid);
-create index log_errors_taskid on log_errors(taskid);
+create index log_infos_taskid on log_infos(taskid);
 create index log_warnings_taskid on log_warnings(taskid);
-create index log_invalids_taskid on log_invalids(taskid);
+create index log_errors_taskid on log_errors(taskid);
+create unique index log_fatals_taskid on log_fatals(taskid);
+create unique index log_invalids_taskid on log_invalids(taskid);
 
 -- Note: to avoid a sequential scan on log fors all the report pages, the following indexes are crucial:
-create index log_fatals_index on log_fatals(category,what,taskid);
-create index log_errors_index on log_errors(category,what,taskid);
+create index log_infos_index on log_infos(category,what,taskid);
 create index log_warnings_index on log_warnings(category,what,taskid);
+create index log_errors_index on log_errors(category,what,taskid);
+create index log_fatals_index on log_fatals(category,what,taskid);
 create index log_invalids_index on log_invalids(category,what,taskid);
