f9d236c00da01c3ace9e3e89c9292e016508d9e9
Deyan Ginev <d.ginev@jacobs-university.de>
2018-08-14 17:48:45 -0400

    improved JSZip logic for previews, using DataURLs for pngs

--- bin/frontend.rs
+++ bin/frontend.rs
@@ -36,13 +36,17 @@ use hyper::header::{ContentLength, ContentType};
 use hyper::Client;
 use hyper::{Method, Request};
 use hyper_tls::HttpsConnector;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
 use rocket::response::status::{Accepted, NotFound};
 use rocket::response::{NamedFile, Redirect};
 use rocket_contrib::Template;
+
 use tokio_core::reactor::Core;
 
 use std::collections::HashMap;
 use std::fs::File;
+use std::io::Cursor;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::str;
@@ -55,6 +59,40 @@ use std::time::Duration;
 use cortex::backend::Backend;
 use cortex::models::{Corpus, Service, Task};
 use cortex::sysinfo;
+
+pub struct CORS();
+
+impl Fairing for CORS {
+  fn info(&self) -> Info {
+    Info {
+      name: "Add CORS headers to requests",
+      kind: Kind::Response,
+    }
+  }
+
+  fn on_response(&self, request: &rocket::Request, response: &mut rocket::Response) {
+    if request.method() == rocket::http::Method::Options
+      || response.content_type() == Some(rocket::http::ContentType::JSON)
+    {
+      response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+      response.set_header(Header::new(
+        "Access-Control-Allow-Methods",
+        "POST, GET, OPTIONS",
+      ));
+      response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+      response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+      response.set_header(Header::new(
+        "Content-Security-Policy-Report-Only",
+        "default-src https:; report-uri /csp-violation-report-endpoint/",
+      ));
+    }
+
+    if request.method() == rocket::http::Method::Options {
+      response.set_header(rocket::http::ContentType::Plain);
+      response.set_sized_body(Cursor::new(""));
+    }
+  }
+}
 
 static UNKNOWN: &'static str = "_unknown_";
 
@@ -179,7 +217,8 @@ fn corpus(corpus_name: String) -> Result<Template, NotFound<String>> {
     global.insert(
       "description".to_string(),
       "An analysis framework for corpora of TeX/LaTeX documents - registered services for "
-        .to_string() + &corpus_name,
+        .to_string()
+        + &corpus_name,
     );
     global.insert("corpus_name".to_string(), corpus_name.to_string());
     global.insert("corpus_description".to_string(), corpus.description.clone());
@@ -522,8 +561,8 @@ fn rocket() -> rocket::Rocket {
         rerun_category,
         rerun_what
       ],
-    )
-    .attach(Template::fairing())
+    ).attach(Template::fairing())
+    .attach(CORS())
 }
 
 fn main() {
@@ -567,7 +606,8 @@ fn serve_report(
       global.insert(
         "description".to_string(),
         "An analysis framework for corpora of TeX/LaTeX documents - statistical reports for "
-          .to_string() + corpus_name,
+          .to_string()
+          + corpus_name,
       );
       global.insert("corpus_name".to_string(), corpus_name.to_string());
       global.insert("corpus_description".to_string(), corpus.description.clone());
