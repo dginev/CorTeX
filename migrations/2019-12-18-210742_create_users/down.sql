62a466a3e3c8e88c8d7ca438450d699b61cec534
Deyan Ginev <deyan.ginev@gmail.com>
2020-01-03 09:06:31 -0500

    allow for tracking user actions

--- migrations/2019-12-18-210742_create_users/down.sql
+++ migrations/2019-12-18-210742_create_users/down.sql
@@ -1,2 +1,3 @@
 DROP TABLE users;
 DROP TABLE user_permissions;
+DROP TABLE user_actions;
--- migrations/2019-12-18-210742_create_users/up.sql
+++ migrations/2019-12-18-210742_create_users/up.sql
@@ -3,6 +3,8 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   display TEXT NOT NULL DEFAULT 'anonymous',
   email varchar(200) NOT NULL,
+  first_seen TIMESTAMP NOT NULL,
+  last_seen TIMESTAMP NOT NULL,
   admin boolean
 );
 create index users_email_idx on users(email);
@@ -17,3 +19,15 @@ CREATE TABLE user_permissions(
   viewer boolean
 );
 create index user_permissions_triple_idx on user_permissions(user_id, corpus_id, service_id);
+
+
+CREATE TABLE user_actions(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  corpus_id INTEGER,
+  service_id INTEGER,
+  action_counter INTEGER NOT NULL DEFAULT 0,
+  last_timestamp TIMESTAMP NOT NULL,
+  location TEXT NOT NULL,
+  description TEXT NOT NULL
+);
