b6fd83dea323dd25199907e0d49e235636b38b95
Deyan Ginev <deyan.ginev@gmail.com>
2020-01-18 13:07:54 -0500

    daemon pid lookup table

---
+++ migrations/2020-01-18-180433_create_daemons/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE daemons (
+  id SERIAL PRIMARY KEY,
+  pid INTEGER NOT NULL,
+  first_seen TIMESTAMP NOT NULL,
+  last_seen TIMESTAMP NOT NULL,
+  name varchar(200) NOT NULL
+);
+create index daemon_name_idx on daemons(name);
