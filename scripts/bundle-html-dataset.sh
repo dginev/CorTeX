dbda2ca76beba208cc6c0e4659ca55dc3b7bbfce
Deyan Ginev <deyan.ginev@gmail.com>
2022-03-12 07:56:49 -0500

    2022

--- scripts/bundle-html-dataset.sh
+++ scripts/bundle-html-dataset.sh
@@ -22,7 +22,7 @@ psql -h $PGADDRESS -U cortex -t -o "$DTPATH/$CORPUSNAME-error-tasks.txt" -c "SEL
 
 echo "2. Unpack into yymm (year-month) directories..."
 
-all_years="91 92 93 94 95 96 97 98 99 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20"
+all_years="91 92 93 94 95 96 97 98 99 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22"
 all_months="01 02 03 04 05 06 07 08 09 10 11 12"
 severitylist="no_problem warning error"
 
